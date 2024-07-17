db79a6f1a1c2d7df07d8348ec6b6bed50099a9aa
Mihir2811 <135440831+Mihir2811@users.noreply.github.com>
2024-07-17 11:43:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+import pickle
+import pandas as pd
+
+# Load the model
+model = pickle.load(open('LinearRegressionModel.pkl', 'rb'))
+data = pd.read_csv('cleaned_car.csv')
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    companies = sorted(data['company'].unique()) 
+    car_models = sorted(data['name'].unique())
+
+    return render_template('index.html', companies=companies, car_models=car_models)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        name = request.form['name']
+        company = request.form['company']
+        year = int(request.form['year'])
+        kms_driven = int(request.form['kms_driven'])
+        fuel_type = request.form['fuel_type']
+        
+        input_data = pd.DataFrame([[name, company, year, kms_driven, fuel_type]], 
+                                  columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'])
+        
+        prediction = model.predict(input_data)
+        
+        return render_template('index.html', prediction_text='Estimated Car Price: ₹{:.2f}'.format(prediction[0]))
+
+if __name__ == '__main__':
+    app.run(debug=True)
