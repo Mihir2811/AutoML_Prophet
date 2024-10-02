c84dd829b4bb4973d1006ab5951ffaeb822d1c23
Mihir2811 <135440831+Mihir2811@users.noreply.github.com>
2024-10-02 20:04:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 # Load the model
-model = pickle.load(open('LinearRegressionModel.pkl', 'rb'))
+model = pickle.load(open('Model.pkl', 'rb'))
 data = pd.read_csv('cleaned_car.csv')
 app = Flask(__name__)
 
@@ -16,19 +16,23 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        name = request.form['name']
-        company = request.form['company']
-        year = int(request.form['year'])
-        kms_driven = int(request.form['kms_driven'])
-        fuel_type = request.form['fuel_type']
+    try:
+        if request.method == 'POST':
+            name = request.form['name']
+            company = request.form['company']
+            year = int(request.form['year'])
+            kms_driven = int(request.form['kms_driven'])
+            fuel_type = request.form['fuel_type']
+            
+            input_data = pd.DataFrame([[name, company, year, kms_driven, fuel_type]], 
+                                      columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'])
+            
+            prediction = model.predict(input_data)
+            
+            return render_template('index3.html', prediction_text='Estimated Car Price: ₹{:.2f}'.format(prediction[0]))
         
-        input_data = pd.DataFrame([[name, company, year, kms_driven, fuel_type]], 
-                                  columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'])
-        
-        prediction = model.predict(input_data)
-        
-        return render_template('index.html', prediction_text='Estimated Car Price: ₹{:.2f}'.format(prediction[0]))
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
